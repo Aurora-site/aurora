6c0e3814d6fd636e7a4df76d0355d3c7934b2cb2
akorzunin <akorzunin123@gmail.com>
2025-01-16 16:25:20 +0300

    feat: added react query

--- src/components/reactive/AuroraNow.tsx
+++ src/components/reactive/AuroraNow.tsx
@@ -1,7 +1,23 @@
+import { useStore } from "@nanostores/react";
 import { ApiService } from "../../api/client";
 import "../../api/config";
+import { queryClient } from "../../stores/query";
+import { useGeolocation } from "../../utils/geo_utils";
+import { useQuery } from "@tanstack/react-query";
 
 export const AuroraNow = () => {
+  const state = useGeolocation();
+  const client = useStore(queryClient);
+  const { data: auroraMapData } = useQuery(
+    {
+      queryKey: ["auroraMap"],
+      queryFn: async () => {
+        const a = await ApiService.apiAuroraMapApiV1AuroraMapGet();
+        return a;
+      },
+    },
+    client,
+  );
   return (
     <div>
       <div>Сияние прямо сейчас</div>
@@ -13,11 +29,10 @@ export const AuroraNow = () => {
         className=""
         onClick={async () => {
           console.log("Show dropdown");
-          const a = await ApiService.apiAuroraMapApiV1AuroraMapGet();
-          console.log(a);
         }}
       >
-        Мурманск
+        Мурманск {state.latitude} {state.longitude}{" "}
+        {auroraMapData?.["Data Format"]}
       </div>
       <div>
         <div>В Вашей геолокации вероятность в ближайший час</div>
