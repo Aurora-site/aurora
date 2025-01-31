1ab55dc99c01727327131e8b98ba1514767703af
akorzunin <akorzunin123@gmail.com>
2025-01-31 22:27:44 +0300

    feat: partial city selector
    #3

---
+++ src/components/reactive/CitySelect.tsx
@@ -0,0 +1,89 @@
+import { ChevronDown, MoveDown, Search } from "lucide-react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "../shadcn/ui/dialog";
+import { useState } from "react";
+import { queryClient } from "../../stores/query";
+import { useStore } from "@nanostores/react";
+import { useQuery } from "@tanstack/react-query";
+import { ApiService } from "../../api/client";
+
+export const CitySelect = () => {
+  const [value, setValue] = useState("a");
+  const [serachQuery, setSerachQuery] = useState("");
+  const client = useStore(queryClient);
+  const { data: probabilityCities } = useQuery(
+    {
+      queryKey: ["probability-cities"],
+      queryFn: async () => {
+        const res = await ApiService.apiAllCitiesApiV1AllCitiesGet();
+        return res;
+      },
+    },
+    client,
+  );
+  return (
+    <div className="">
+      <Dialog>
+        <DialogTrigger asChild>
+          <div className="flex items-center gap-4 py-5 text-2xl underline">
+            {serachQuery || "Мурманск"}
+            <ChevronDown className="h-4 w-4" />
+          </div>
+        </DialogTrigger>
+        <DialogContent className="flex flex-col">
+          <DialogHeader>
+            <DialogTitle className="giga-text text-center text-white">
+              Геолокация
+            </DialogTitle>
+            <DialogDescription className="text-lg">
+              На широте какого города Вы находитесь?
+            </DialogDescription>
+          </DialogHeader>
+          <div className="py-5">
+            <div className="mb-5 flex items-center gap-3 rounded-2xl bg-slate-300 px-5 py-2 text-black">
+              <Search />
+              <input
+                placeholder="Мурманск"
+                className="w-full focus:outline-none"
+                value={serachQuery}
+                onChange={(e) => setSerachQuery(e.target.value)}
+              />
+            </div>
+            <div className="grid grid-cols-2 gap-y-2 xl:w-[50%]">
+              {probabilityCities
+                ?.filter((v) =>
+                  v.name.toLowerCase().includes(serachQuery.toLowerCase()),
+                )
+                .map((c, i) => (
+                  <div
+                    key={i.toString()}
+                    onClick={() => setSerachQuery(c.name)}
+                    className="cursor-pointer"
+                  >
+                    {c.name}
+                  </div>
+                ))}
+            </div>
+          </div>
+          <button
+            onClick={() => {
+              console.log(
+                probabilityCities?.find((v) => v.name == serachQuery),
+              );
+            }}
+            className="mt-auto mb-0 flex cursor-pointer self-center rounded-full bg-slate-300 px-5 py-2 text-black"
+          >
+            Ok
+          </button>
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
+};
