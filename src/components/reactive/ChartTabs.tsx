d3bcdc520ec8f7d6d2c0b037446c122f2e98e1a4
akorzunin <akorzunin123@gmail.com>
2025-02-01 00:24:50 +0300

    Дизайн графиков #5

--- src/components/reactive/ChartTabs.tsx
+++ src/components/reactive/ChartTabs.tsx
@@ -2,6 +2,10 @@ import * as Tabs from "@radix-ui/react-tabs";
 import React from "react";
 import { KpGraph3 } from "./KpGraph3";
 import { KpGraph27 } from "./KpGraph27";
+import { cn } from "../../utils/cn";
+
+const className =
+  "w-40 py-1 rounded-full border-0 transition-colors duration-200 ease-in-out ";
 
 export const ChartTabs = () => {
   const [activeTab, setActiveTab] = React.useState("tab1");
@@ -10,23 +14,32 @@ export const ChartTabs = () => {
       value={activeTab}
       onValueChange={setActiveTab}
       defaultValue="tab1"
+      className="flex-col"
     >
       <Tabs.List
-        className="flex gap-5 p-5"
+        className="flex justify-center gap-5 p-5"
         aria-label="Select a tab with kp graph"
       >
-        <Tabs.Trigger
-          className={`border-b-2 transition-colors duration-200 ease-in-out ${activeTab === "tab1" ? "border-b-black" : ""}`}
-          value="tab1"
-        >
-          За 3 дня
-        </Tabs.Trigger>
-        <Tabs.Trigger
-          className={`border-b-2 transition-colors duration-200 ease-in-out ${activeTab === "tab2" ? "border-b-black" : ""}`}
-          value="tab2"
-        >
-          За 27 дней
-        </Tabs.Trigger>
+        <div className="flex justify-between gap-1 rounded-full border-2">
+          <Tabs.Trigger
+            className={cn(
+              className,
+              activeTab === "tab1" && "bg-tab-active text-white",
+            )}
+            value="tab1"
+          >
+            3 суток
+          </Tabs.Trigger>
+          <Tabs.Trigger
+            className={cn(
+              className,
+              activeTab === "tab2" && "bg-tab-active text-white",
+            )}
+            value="tab2"
+          >
+            27 суток
+          </Tabs.Trigger>
+        </div>
       </Tabs.List>
       <Tabs.Content value="tab1">
         <KpGraph3 />
