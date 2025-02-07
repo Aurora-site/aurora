3daee36159cdb5848d102afcf731eebb7af7aa32
akorzunin <akorzunin123@gmail.com>
2025-02-07 16:04:23 +0300

    fix: i18n lang types + /app page

--- src/components/reactive/LanguagePicker.tsx
+++ src/components/reactive/LanguagePicker.tsx
@@ -1,15 +1,23 @@
-import { flagUrls } from "../../i18n/ui";
+import { useEffect } from "react";
+import { ui } from "../../i18n/ui";
+import { getLangFromHref } from "../../i18n/utils";
+import { localeAtom } from "../../stores/locale";
 
 export const LanguagePicker = () => {
+  const t = getLangFromHref(window.location.href);
+  useEffect(() => {
+    localeAtom.set(t);
+  }, []);
   return (
     <div className="flex-coloumn items-center space-y-[20px] pt-[4px] lg:space-y-[8px]">
-      {Object.entries(flagUrls).map(([lang]) => (
+      {(Object.keys(ui) as (keyof typeof ui)[]).map((lang) => (
         <a
           key={lang}
           href={`/${lang}/`}
           className="flex items-center justify-center lg:justify-end"
+          title={lang}
         >
-          <img src={flagUrls[lang]} alt={lang} width="30" />
+          <img src={ui[lang].icon} alt={lang} width="30" />
         </a>
       ))}
     </div>
--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -1,19 +1,15 @@
-export const flagUrls: Record<string, string> = {
-  cn: "https://flagcdn.com/w40/cn.png",
-  ru: "https://flagcdn.com/w40/ru.png",
-  en: "https://flagcdn.com/w40/us.png",
-};
-
 export const defaultLang = "ru";
 
 export const ui = {
   ru: {
+    icon: "https://flagcdn.com/w40/ru.png",
     "layout.title": "Северное сияние",
     "description.title": "Северное сияние",
     "description.how": "Карта и реальная вероятность",
     "aurora.probability": "В выбранной геолокации вероятность в ближайший час",
   },
   en: {
+    icon: "https://flagcdn.com/w40/us.png",
     "layout.title": "Southern Lights",
     "description.title": "Southern Lights",
     "description.how": "How to see?",
@@ -21,6 +17,8 @@ export const ui = {
       "In your location, the probability of aurora in the next hour:",
   },
   cn: {
+    icon: "https://flagcdn.com/w40/cn.png",
+    "description.title": "极光",
     "aurora.probability": "在您的位置，下一小时的极光概率：",
   },
 } as const;
