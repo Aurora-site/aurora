dc5b0636071f685f674842c5f84cac17ae454489
akorzunin <akorzunin123@gmail.com>
2025-02-09 17:52:26 +0300

    fix: i18n utils

--- src/components/reactive/LanguagePicker.tsx
+++ src/components/reactive/LanguagePicker.tsx
@@ -1,22 +1,11 @@
 import { useEffect } from "react";
 import { ui } from "../../i18n/ui";
-import { getLangFromHref } from "../../i18n/utils";
+import { getLangFromHref, getLocalizedUrl } from "../../i18n/utils";
 import { localeAtom } from "../../stores/locale";
 
 export const LanguagePicker = () => {
   useEffect(() => {
-    // Получаем текущий язык из localStorage или из URL, если в localStorage его нет
-    const savedLang = localStorage.getItem("lang");
-    const lang =
-      savedLang && savedLang in ui
-        ? savedLang
-        : getLangFromHref(window.location.href);
-
-    // Устанавливаем язык в atom
-    localeAtom.set(lang);
-
-    // Сохраняем выбранный язык в localStorage
-    localStorage.setItem("lang", lang);
+    localeAtom.set(getLangFromHref(window.location.href));
   }, []); // Выполнится только один раз при монтировании компонента
 
   return (
@@ -24,18 +13,9 @@ export const LanguagePicker = () => {
       {(Object.keys(ui) as (keyof typeof ui)[]).map((lang) => (
         <a
           key={lang}
-          href={`/${lang}/`} // Ссылка для перехода на выбранный язык
+          href={getLocalizedUrl(lang, window.location.pathname)} // Ссылка для перехода на выбранный язык
           className="flex items-center justify-center lg:justify-end"
           title={lang}
-          onClick={(e) => {
-            // Останавливаем переход по ссылке
-            e.preventDefault();
-            // Сохраняем выбранный язык в localStorage и меняем atom
-            localStorage.setItem("lang", lang);
-            localeAtom.set(lang);
-            // Перезагружаем страницу с новым языком
-            window.location.href = `/${lang}${window.location.pathname.slice(3)}`;
-          }}
         >
           <img src={ui[lang].icon} alt={lang} width="30" />
         </a>
--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -1,23 +1,25 @@
 import { useStore } from "@nanostores/react";
 import { ui, defaultLang } from "./ui";
 import type { WritableAtom } from "nanostores";
+import { getRelativeLocaleUrl } from "astro:i18n";
+
+export type Lang = keyof typeof ui;
 
 export function getLangFromUrl(url: URL) {
   const [, lang] = url.pathname.split("/");
-  if (lang in ui) return lang as keyof typeof ui;
+  if (lang in ui) return lang as Lang;
   return defaultLang;
 }
 
 export function getLangFromHref(href: string) {
   for (const i of href.split("/")) {
-    if (i in ui) return i as keyof typeof ui;
+    if (i in ui) return i as Lang;
   }
   return defaultLang;
 }
 
-export function useTranslations(lang: keyof typeof ui) {
+export function useTranslations(lang: Lang) {
   return function t(key: keyof (typeof ui)[typeof defaultLang]) {
-    // @ts-ignore-line
     return ui[lang][key] || ui[defaultLang][key];
   };
 }
@@ -26,8 +28,8 @@ export function useTranslations(lang: keyof typeof ui) {
 type Astro = { currentLocale?: string };
 
 export function useAstroLocale(a: Astro) {
-  const l = a.currentLocale as keyof typeof ui;
-  if (l in ui) return l as keyof typeof ui;
+  const l = a.currentLocale as Lang;
+  if (l in ui) return l as Lang;
   return defaultLang;
 }
 
@@ -41,32 +43,8 @@ export function useAstroTranslations(a: Astro) {
 }
 
 // Функция, которая добавляет текущий язык в путь URL
-export function getLocalizedUrl(path: string, currentLang: keyof typeof ui) {
-  const parts = path.split("/").filter(Boolean); // Убираем пустые элементы
-
-  // Если язык не указан, просто возвращаем путь без изменений
-  if (!currentLang) {
-    return path;
-  }
-
-  if (parts[0] in ui) {
-    parts[0] = currentLang; // Заменяем существующий язык
-  } else {
-    parts.unshift(currentLang); // Добавляем язык в начало
-  }
-
-  return "/" + parts.join("/");
-}
-
-// Функция для смены языка, но с сохранением текущей страницы
-export function switchLanguage(newLang: keyof typeof ui, currentUrl: string) {
-  const parts = currentUrl.split("/").filter(Boolean);
-
-  if (parts[0] in ui) {
-    parts[0] = newLang; // Заменяем язык
-  } else {
-    parts.unshift(newLang); // Добавляем язык в начало
-  }
-
-  return "/" + parts.join("/");
+export function getLocalizedUrl(lang: Lang, path: string) {
+  const p = path.split("/").filter(Boolean); // Убираем пустые элементы
+  const lPath = p.length >= 2 ? p.slice(-1)[0] : undefined;
+  return getRelativeLocaleUrl(lang, lPath);
 }
