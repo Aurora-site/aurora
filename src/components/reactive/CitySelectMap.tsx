a652a69d7d7ef26631442e1d47cba7b0aae04b2d
akorzunin <akorzunin123@gmail.com>
2025-03-05 09:36:54 +0300

    fix: refactored types in CitySelect

--- src/components/reactive/CitySelectMap.tsx
+++ src/components/reactive/CitySelectMap.tsx
@@ -1,5 +1,4 @@
-// @ts-nocheck
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 import VectorSource from "ol/source/Vector";
 import VectorLayer from "ol/layer/Vector";
 import Feature from "ol/Feature";
@@ -9,66 +8,65 @@ import { Style, Circle as CircleStyle, Fill, Text } from "ol/style";
 import { useStore } from "@nanostores/react";
 import { cityAtom } from "../../stores/citiy";
 import { useGeolocation } from "../../utils/geo_utils";
+import Map from "ol/Map";
 
-export function CitySelectMap({ map }) {
+export function CitySelectMap({ map }: { map: Map | null }) {
   const city = useStore(cityAtom); // Получаем выбранный город
-  const cityLayerRef = useRef<VectorLayer<
-    VectorSource<Feature<Point>>,
-    Feature<Point>
-  > | null>(null);
-  const userLayerRef = useRef(null);
+  type CityLayer = VectorLayer<VectorSource<Feature<Point>>, Feature<Point>>;
+  const cityLayerRef = useRef<CityLayer>(null);
+  const userLayerRef = useRef<VectorLayer>(null);
   const location = useGeolocation({ enableHighAccuracy: true });
 
   useEffect(() => {
-    if (map) {
-      let cityLayer = cityLayerRef.current;
+    if (!map) return;
+    let cityLayer = cityLayerRef.current;
 
-      // Если слоя еще нет, создаем его
-      if (!cityLayer) {
-        const citySource = new VectorSource<Feature<Point>>();
+    // Если слоя еще нет, создаем его
+    if (!cityLayer) {
+      const citySource = new VectorSource<Feature<Point>>();
 
-        // Создаем начальную feature для города
-        const feature = new Feature({
-          geometry: new Point(fromLonLat([city.long, city.lat])),
-          name: city.name,
-        });
-        citySource.addFeature(feature);
+      // Создаем начальную feature для города
+      const feature = new Feature({
+        geometry: new Point(fromLonLat([city.long, city.lat])),
+        name: city.name,
+      });
+      citySource.addFeature(feature);
 
-        // Создаем слой с городом
-        cityLayer = new VectorLayer({
-          source: citySource,
-          style: (feature) =>
-            new Style({
-              image: new CircleStyle({
-                radius: 3,
-                fill: new Fill({ color: "black" }),
-              }),
-              text: new Text({
-                text: feature.get("name"),
-                font: "12px 'Montserrat', sans-serif",
-                fill: new Fill({ color: "black" }),
-                offsetY: -12,
-              }),
+      // Создаем слой с городом
+      cityLayer = new VectorLayer({
+        source: citySource,
+        style: (feature) =>
+          new Style({
+            image: new CircleStyle({
+              radius: 3,
+              fill: new Fill({ color: "black" }),
             }),
-        });
+            text: new Text({
+              text: feature.get("name"),
+              font: "12px 'Montserrat', sans-serif",
+              fill: new Fill({ color: "black" }),
+              offsetY: -12,
+            }),
+          }),
+      });
 
-        cityLayer.setZIndex(3);
-        cityLayer.set("name", "cityLayer");
-        map.addLayer(cityLayer);
+      cityLayer.setZIndex(3);
+      cityLayer.set("name", "cityLayer");
+      map.addLayer(cityLayer);
 
-        // Сохраняем ссылку на слой для дальнейших изменений
-        cityLayerRef.current = cityLayer;
-      } else {
-        // Если слой уже существует, обновляем данные
-        const citySource = cityLayer.getSource();
-        const features = citySource.getFeatures();
+      // Сохраняем ссылку на слой для дальнейших изменений
+      cityLayerRef.current = cityLayer;
+    } else {
+      // Если слой уже существует, обновляем данные
+      const citySource = cityLayer.getSource();
+      if (!citySource) return;
+      const features = citySource.getFeatures();
 
-        // Находим существующую feature и обновляем её
-        if (features.length > 0) {
-          const feature = features[0];
-          feature.setGeometry(new Point(fromLonLat([city.long, city.lat])));
-          feature.set("name", city.name);
-        }
+      // Находим существующую feature и обновляем её
+      if (features.length > 0) {
+        const feature = features[0];
+        feature.setGeometry(new Point(fromLonLat([city.long, city.lat])));
+        feature.set("name", city.name);
       }
     }
   }, [map, city]); // Обновляем слой, когда меняется выбранный город
@@ -92,26 +90,25 @@ export function CitySelectMap({ map }) {
               fill: new Fill({ color: "blue" }),
             }),
           }),
+          zIndex: 5,
         });
 
-        userLayer.setZIndex(4);
-        userLayer.set("name", "userLayer");
         map.addLayer(userLayer);
         userLayerRef.current = userLayer;
       }
 
       const userSource = userLayer.getSource();
+      if (userSource === null) return;
       userSource.clear();
-
-      const userFeature = new Feature({
-        geometry: new Point(
-          fromLonLat([location.longitude, location.latitude]),
-        ),
-      });
-
-      userSource.addFeature(userFeature);
+      userSource.addFeature(
+        new Feature({
+          geometry: new Point(
+            fromLonLat([location.longitude, location.latitude]),
+          ),
+        }),
+      );
     }
-  }, [map, location.latitude, location.longitude, location.loading]);
+  }, [map, location]);
 
   return null;
 }
